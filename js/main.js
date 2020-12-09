81d2cf5f08e58b3fbabb2b2138c2a4dbb215f3c3
Dionysus <mustaihmelnen@gmail.com>
2020-12-09 19:20:15 +0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -4,7 +4,31 @@ $(function(){
     slidesToShow: 3,
     slidesToScroll: 3,
     arrows: false,
-    dots: true
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1109,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 735,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
   
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active')
+  });
 });
